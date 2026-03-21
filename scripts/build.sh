057cfdb3aa214c331b56db6d4dbbf2617224e0a5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-21 22:11:30 +0100

    script: Build

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cargo build --locked --target wasm32-unknown-unknown --release -p minter
+
+gzip -c target/wasm32-unknown-unknown/release/minter.wasm >target/wasm32-unknown-unknown/release/minter.wasm.gz
