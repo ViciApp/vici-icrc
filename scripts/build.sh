54f969801db35e1e8ce3f87c30df2cd52a370e82
Antonio Ventilii <antonioventilii@gmail.com>
2026-03-22 00:37:34 +0100

    fix: Build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,5 @@
 cargo build --locked --target wasm32-unknown-unknown --release -p minter
 
 gzip -c target/wasm32-unknown-unknown/release/minter.wasm >target/wasm32-unknown-unknown/release/minter.wasm.gz
+
+./scripts/download.ledger.sh
